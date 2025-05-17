637e1975e608cbf3fb5b7228417b64bd448bda1a
Boukhezzarfahim <boukhezzarfahim3@gmail.com>
2025-05-17 19:05:10 +0100

    School Levels

--- app/Http/Controllers/NiveauScolaireController.php
+++ app/Http/Controllers/NiveauScolaireController.php
@@ -2,14 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\NiveauScolaire;
 use Illuminate\Http\Request;
 
 class NiveauScolaireController extends Controller
 {
        public function index() {
 
+        $niveaux_scolaires = NiveauScolaire::orderBy("nom", "ASC")->get();
 
-        return inertia("NiveauScolaire/Index");
+
+        return inertia("NiveauScolaire/Index", [
+
+            "niveauScolaires" => $niveaux_scolaires
+        ]);
 
     }
 
--- resources/js/Pages/NiveauScolaire/Index.vue
+++ resources/js/Pages/NiveauScolaire/Index.vue
@@ -1,4 +1,63 @@
 <template>
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">Liste de tous les niveaux scolaires</h1>
+        </div>
+      </div>
+    </div>
+  </div>
 
-<h1>azul </h1>
+  <div class="content">
+    <div class="container-fluid">
+      <div class="row">
+
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-header">
+              <h3 class="card-title"></h3>
+              <div class="card-tools">
+                <ul class="pagination pagination-sm float-right">
+                  <li class="page-item"><a class="page-link" href="#">«</a></li>
+                  <li class="page-item"><a class="page-link" href="#">1</a></li>
+                  <li class="page-item"><a class="page-link" href="#">2</a></li>
+                  <li class="page-item"><a class="page-link" href="#">3</a></li>
+                  <li class="page-item"><a class="page-link" href="#">»</a></li>
+                </ul>
+              </div>
+            </div>
+
+            <div class="card-body p-0">
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th>Niveau scolaire</th>
+                    <th style="width: 100px"></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="niveauScolaire in props.niveauScolaires">
+                    <td>{{niveauScolaire.nom}}</td>
+                    <td>
+                          <div class="d-flex justify-items-center">
+                          <button class="btn btn-info mr-2"><i class="fas fa-pen"></i></button>
+                          <button class="btn btn-danger"><i class="fas fa-trash"></i></button>
+                          </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script setup>
+  const props = defineProps({
+    niveauScolaires: Array
+  })
+</script>
